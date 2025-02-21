66bcf943332a5bfd4c90812bb5a9344a75b3479b
AlexArancibia <alexanderarancibia37@gmail.com>
2025-02-21 17:08:29 -0500

    proxy true

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://canchalibrecms.emetstudio.com',
-  proxy: true,
+  url: 'https://canchalibrecms.emetstudio.com', // URL con HTTPS
+  proxy: true,  // Indica que está detrás de un proxy
+  trustProxy: true, // Asegura que reconozca la conexión HTTPS
   app: {
     keys: env.array('APP_KEYS'),
   },
