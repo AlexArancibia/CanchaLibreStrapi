97d74a9ad657adb7046e2599f1e2b4a8277aa7cf
AlexArancibia <alexanderarancibia37@gmail.com>
2025-02-21 14:02:32 -0500

    plugins updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+      providers: {
+        google: {
+          clientId: env("GOOGLE_CLIENT_ID"),
+          clientSecret: env("GOOGLE_CLIENT_SECRET"),
+          redirectUri: "https://canchalibrecms.emetstudio.com/api/connect/google/callback",
+        },
+      },
+      
+    },
+  },
+});
