71d7e80439de665b7919150a92caf55c72a83e4e
AlexArancibia <alexanderarancibia37@gmail.com>
2025-02-21 14:20:47 -0500

    desable secure temporally

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,39 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000', // Frontend local
+        'https://canchalibrecms.emetstudio.com', // Backend de Strapi
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      cookie: {
+        secure: false, // Deshabilitar cookies seguras en desarrollo
+      },
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
